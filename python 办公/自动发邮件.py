b757d3b2cd5205c8a4ad4527ebcfb62218aa6545
kaka4710 <57857117+kaka4710@users.noreply.github.com>
2019-11-17 21:44:42 +0800

    Create 自动发邮件.py

---
+++ python 办公/自动发邮件.py
@@ -0,0 +1,19 @@
+# 导入的库：
+'''
+import smtplib
+from smtplib import SMTP.SSl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMIMultipart
+from email.header import Header
+
+host_sever = 'smtp.sina.com'
+sender_sina = 'python@sina.com'
+pwd = 'python1234'
+
+sender_sina_mail = 'python@sina.com'
+reveiver = 'python@sina.com'
+mail_title = 'python自动发送邮件'
+mail_content = '你好，这是python自动邮件'
+msg = MIMEMultipart()
+msg['Sub']
+'''
